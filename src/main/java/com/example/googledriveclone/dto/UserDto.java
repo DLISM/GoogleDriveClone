98d43b38deef3340d832569069949cfa200a8727
Javohir <ismoilov92@gmail.com>
2023-04-19 22:08:29 +0300

    validation username

--- src/main/java/com/example/googledriveclone/dto/UserDto.java
+++ src/main/java/com/example/googledriveclone/dto/UserDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 @Data
 @AllArgsConstructor
@@ -12,6 +13,7 @@ import jakarta.validation.constraints.Size;
 public class UserDto  {
     @NotEmpty(message = "Имя пользователя не должен быть пустым")
     @Size(min = 3, max = 30, message = "Имя пользователя должно содержать от 3 до 30 символов")
+    @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Имя пользователя может содержать только буквы, цифры и знак подчеркивания")
     private String username;
 
     @NotEmpty(message = "Пароль не должен быть пустым")
